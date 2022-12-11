56a0b5cca33660f19ba631c666a532b2b871fb4d
Victor <winnie_puh16@outlook.com>
2022-12-11 06:33:17 -0600

    Arreglo

--- app/Http/Controllers/Parque/ParqueController.php
+++ app/Http/Controllers/Parque/ParqueController.php
@@ -4,31 +4,31 @@ namespace App\Http\Controllers\Parque;
 
 use App\Http\Controllers\Controller;
 use App\Models\ModelosParque\Parque;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ParqueController extends Controller
 {
-    public function addParque(Request $request){
+    public function addParque(Request $request)
+    {
         $validacion = Validator::make(
-            $request->all(),[
-                'nombre'                => "required|string|max:25",
-                'reglas'                => "required|string|max:1000",
-                'medida_largoTerreno'   => "required|integer",
-                'medida_anchoTerreno'   => "required|integer",
-                'cantidad_sensores'     => 'required|integer',
-                'cantidad_entradas'     => 'required|integer',
-                'cantidad_salidas'      => 'required|integer'
+            $request->all(), [
+                'nombre' => "required|string|max:25",
+                'reglas' => "required|string|max:1000",
+                'medida_largoTerreno' => "required|integer",
+                'medida_anchoTerreno' => "required|integer",
+                'cantidad_sensores' => 'required|integer',
+                'cantidad_entradas' => 'required|integer',
+                'cantidad_salidas' => 'required|integer',
             ]
         );
-        if($validacion->fails()){
+        if ($validacion->fails()) {
             return response()->json([
-                "status"    => 400,
-                "msg"       => "No se cumplieron las validaciones",
-                "error"     => $validacion->errors(),
-                "data"      => null
+                "status" => 400,
+                "msg" => "No se cumplieron las validaciones",
+                "error" => $validacion->errors(),
+                "data" => null,
             ], 400);
         }
 
@@ -43,29 +43,30 @@ class ParqueController extends Controller
         $parque->cantidad_salidas = $request->cantidad_salidas;
         $parque->save();
 
-        if($parque->save()){
+        if ($parque->save()) {
             return response()->json([
-                "status"        => 201,
-                "msg"           => "Se insertaron datos de manera satisfactoria",
-                "error"         => null,
-                "data"          => $parque
+                "status" => 201,
+                "msg" => "Se insertaron datos de manera satisfactoria",
+                "error" => null,
+                "data" => $parque,
             ], 201);
         }
     }
 
-    public function getAllParques(Request $request){
+    public function getAllParques(Request $request)
+    {
         $id = $request->url();
         $parque = DB::table('parques')
             ->join('users', 'parques.dueño_id', '=', 'users.id')
             ->where('status', true)->where('dueño_id', $id)
             ->get();
         return response()->json([
-            "status"=>200,
-            "msg"=>"Informacion localizada",
-            "error"=>null,
+            "status" => 200,
+            "msg" => "Informacion localizada",
+            "error" => null,
             // "data"=>Persona::all()
-            "data"=>$parque
-        ],200);
+            "data" => $parque,
+        ], 200);
         // Parque::where('status', true)->get()
     }
 }
